f8ee93cd7e3b0e4d2b0e29ad3a2d91fd6ede6805
xinchanghao <1433405285@qq.com>
2016-09-28 22:49:31 +0800

    数据库配置更改

--- ptngo/settings.py
+++ ptngo/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ptngo',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': '123456',
         'HOST':'localhost',
         'PORT':'3306',
     }
