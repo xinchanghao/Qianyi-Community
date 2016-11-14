56780697e0dd5babc8990e579f274fc12fb8846a
xinchanghao <1433405285@qq.com>
2016-11-14 19:14:20 +0800

    数据库更改，增加task表；用户主页调整

--- app/models.py
+++ app/models.py
@@ -44,15 +44,30 @@ class User(models.Model):
 	user_longi =models.CharField(u'经度',max_length=25,null=True)
 	user_lati =models.CharField(u'纬度',max_length=25,null=True)
 	user_forbid = models.NullBooleanField(u'是否黑名单',null=True,default="0")
-	user_photo= models.CharField(u'头像',max_length=256,null=True)
-	user_descripe= models.CharField(u'描述',max_length=256,null=True)
+	user_photo= models.CharField(u'头像',max_length=256,null=True,default="project-2.jpg")
+	user_descripe= models.CharField(u'描述',max_length=256,null=True,default="尚未更新...")
 	user_password =models.CharField(u'瀵嗙爜',max_length=16)
 	user_email =models.EmailField(u'閭',max_length=30)
-	user_sex =models.TextField(u'鎬у埆',max_length=5,null=True)
-	user_addr =models.CharField(u'绠�浠�',max_length=256,null=True)
+	user_sex =models.TextField(u'鎬у埆',max_length=5,null=True,default="尚未更新...")
+	user_addr =models.CharField(u'绠�浠�',max_length=256,null=True,default="尚未更新...")
 	user_birth =models.DateTimeField(u'鍑虹敓骞存湀',editable=True,null=True,auto_now_add=True)
-	user_sign=models.CharField(u'签名',max_length=256,null=True)
-	user_intro=models.CharField(u'介绍',max_length=512,null=True)
+	user_sign=models.CharField(u'签名',max_length=256,null=True,default="尚未更新...")
+	user_intro=models.CharField(u'介绍',max_length=512,null=True,default="尚未更新...")
+
+	#update_time =models.DateTimeField(u'鏇存柊鏃堕棿',auto_now=True,null=True)
+
+	def __str__(self):
+		return self.title
+	
+	
+# 瀹氫箟鐢ㄦ埛绫绘ā鍨�
+from django.utils.encoding import python_2_unicode_compatible
+@python_2_unicode_compatible
+#姝ゅ鍏煎python2
+class Task(models.Model):
+	user_name =models.CharField(u'濮撳悕',max_length=256)
+	user_task= models.CharField(u'任务',max_length=256,null=True,default="尚未更新...")
+
 
 	#update_time =models.DateTimeField(u'鏇存柊鏃堕棿',auto_now=True,null=True)
 
